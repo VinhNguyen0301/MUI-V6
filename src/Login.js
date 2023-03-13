19a592fd5f90f1a4401daae398e132f8487bbea1
VinhNguyen <vinh.nguyen@dedoco.com>
2023-03-13 09:36:56 +0700

    Add layer spotify

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css";
+import { loginUrl } from "./spotify.js";
 
 const Login = () => {
   return (
@@ -8,7 +9,7 @@ const Login = () => {
         src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
         alt="Spotify logo"
       />
-      <a href="#">LOGIN WITH SPOTIFY</a>
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 };
